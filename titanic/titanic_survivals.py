6dc8aa5567fa215cba6d66a539007de8c0bc3340
Anton Bauer <abton.bauer@gmail.com>
2023-02-07 21:13:34 +0100

    implemented features/label split

--- titanic/titanic_survivals.py
+++ titanic/titanic_survivals.py
@@ -1,4 +1,5 @@
 import os
+from typing import Sequence
 
 import opendatasets as od
 import pandas as pd
@@ -7,14 +8,18 @@ import tensorflow as tf
 dataset_url = 'https://www.kaggle.com/c/titanic/data'
 dataset_path = '../data/'
 
+
 def download_dataset(url: str, path: str) -> pd.DataFrame:
     od.download(url, path)
-    return pd.read_csv(os.path.join(dataset_path, 'titanic', 'train.csv')) 
+    return pd.read_csv(os.path.join(dataset_path, 'titanic', 'train.csv'))
 
 
-def print_dataset_info(dataset: pd.DataFrame):
+def print_dataset_info(dataset: pd.DataFrame, description: str = ''):
+    print(description)
     print(dataset.describe())
     print(dataset.head())
+    print()
+
 
 def split_dataset(dataset: pd.DataFrame, split_ratio: float) -> tuple[pd.DataFrame, pd.DataFrame]:
     dataset_copy = dataset.copy()
@@ -22,6 +27,20 @@ def split_dataset(dataset: pd.DataFrame, split_ratio: float) -> tuple[pd.DataFra
     test_dataset = dataset_copy.drop(train_dataset.index)
     return train_dataset, test_dataset
 
+
+def split_to_features_and_labels(dataset: pd.DataFrame, features, label) -> tuple[pd.DataFrame, pd.DataFrame]:
+    y = dataset[label]
+    X = dataset[features]
+    return X, y
+
+
 titanic_data = download_dataset(dataset_url, dataset_path)
-print_dataset_info(titanic_data)
+print_dataset_info(titanic_data, 'Titanic data')
 titanic_train, titanic_test = split_dataset(titanic_data, 0.8)
+
+label = 'Survived'
+features = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+X_train, y_train = split_to_features_and_labels(titanic_train, features, label)
+
+print_dataset_info(X_train, 'Train features')
+print_dataset_info(y_train, 'Train labels')
