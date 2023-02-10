29d925f7a71851ae03f9cb1959662315e2b52bea
Anton Bauer <abton.bauer@gmail.com>
2023-02-10 21:13:03 +0100

    add split into features/labels

--- titanic/helpers.py
+++ titanic/helpers.py
@@ -2,7 +2,7 @@ import os
 
 import opendatasets as od
 import pandas as pd
-import tensorflow as tf
+from pandas import Series
 
 
 def print_dataset_info(dataset: pd.DataFrame, description: str = ''):
@@ -12,10 +12,17 @@ def print_dataset_info(dataset: pd.DataFrame, description: str = ''):
     print()
 
 
-def download_dataset(url: str, dataset_path: str, print_dataset=False):
+def download_dataset(url: str, dataset_path: str, print_dataset=False) -> pd.DataFrame:
     od.download(url, dataset_path)
     dataset = pd.read_csv(os.path.join(dataset_path, 'titanic', 'train.csv'))
     if print_dataset:
         print_dataset_info(dataset, 'Titanic data')
 
-    return tf.data.Dataset.from_tensor_slices(dict(dataset))
+    return dataset
+
+
+def split_into_features_and_label(dataset: pd.DataFrame, label) -> tuple[pd.DataFrame, Series]:
+    features = dataset.copy()
+    label = features.pop(label)
+
+    return features, label
--- titanic/titanic_survivals.py
+++ titanic/titanic_survivals.py
@@ -3,4 +3,9 @@ import helpers
 dataset_url = 'https://www.kaggle.com/c/titanic/data'
 dataset_path = '../data/'
 
-titanic_data = helpers.download_dataset(dataset_url, dataset_path, print_dataset=True)
+titanic_data = helpers.download_dataset(
+    dataset_url, dataset_path, print_dataset=True)
+titanic_features, titanic_label = helpers.split_into_features_and_label(
+    titanic_data, 'Survived')
+
+helpers.print_dataset_info(titanic_features, 'Titanic features')
