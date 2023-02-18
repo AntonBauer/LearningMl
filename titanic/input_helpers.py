4466ca6622f633e5bd080143727dae45c4d1b7a8
Anton Bauer <abton.bauer@gmail.com>
2023-02-18 09:32:38 +0100

    create preprocessing head

--- titanic/input_helpers.py
+++ titanic/input_helpers.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pandas as pd
 import tensorflow as tf
-import numpy as np
+
 
 def _to_input_type(column_type):
     if column_type == object:
@@ -9,7 +10,7 @@ def _to_input_type(column_type):
         return tf.float32
 
 
-def build_symbolic_inputs(dataset: pd.DataFrame) -> dict[str, tf.keras.Input]:
+def _build_symbolic_inputs(dataset: pd.DataFrame) -> dict[str, tf.keras.Input]:
     inputs = {}
     for name, column in dataset.items():
         input_type = _to_input_type(column.dtype)
@@ -18,26 +19,42 @@ def build_symbolic_inputs(dataset: pd.DataFrame) -> dict[str, tf.keras.Input]:
     return inputs
 
 
-def extract_numerical_inputs(inputs: dict[str, tf.keras.Input]) -> dict[str, tf.keras.Input]:
+def _extract_numerical_inputs(inputs: dict[str, tf.keras.Input]) -> dict[str, tf.keras.Input]:
     return {name: input for name, input in inputs.items()
             if input.dtype == tf.float32}
 
-def extract_string_inputs(inputs: dict[str, tf.keras.Input]) -> dict[str, tf.keras.Input]:
+
+def _extract_string_inputs(inputs: dict[str, tf.keras.Input]) -> dict[str, tf.keras.Input]:
     return {name: input for name, input in inputs.items()
             if input.dtype != tf.float32}
 
-def normalize_inputs(inputs: dict[str, tf.keras.Input], dataset):
+
+def _normalize_inputs(inputs: dict[str, tf.keras.Input], dataset):
     denormalized = tf.keras.layers.Concatenate(list(inputs.values()))
     normalization_layer = tf.keras.layers.Normalization()
     normalization_layer.adapt(np.array(dataset[inputs.keys()]))
 
     return normalization_layer(denormalized)
 
-def create_category_encodings(inputs: dict[str, tf.keras.Input], dataset):
+
+def _create_category_encodings(inputs: dict[str, tf.keras.Input], dataset):
     for name, input in inputs.items():
-        lookup = tf.keras.layers.StringLookup(vocabulary=np.unique(dataset[name]))
-        category_encoding = tf.keras.layers.CategoryEncoding(num_tokens=lookup.vocabulary_size())
+        lookup = tf.keras.layers.StringLookup(
+            vocabulary=np.unique(dataset[name]))
+        category_encoding = tf.keras.layers.CategoryEncoding(
+            num_tokens=lookup.vocabulary_size())
 
         yield category_encoding(lookup(input))
 
-# def create_preprocessing_inputs(dataset: pd.DataFrame):
+
+def create_preprocessing_head(dataset: pd.DataFrame) -> tf.keras.Model:
+    symbolic_inputs = _build_symbolic_inputs(dataset)
+    numerical_inputs = _extract_numerical_inputs(symbolic_inputs)
+    string_inputs = _extract_string_inputs(symbolic_inputs)
+
+    normalized_layer = _normalize_inputs(numerical_inputs, dataset)
+    category_encodings_layer = list(
+        _create_category_encodings(string_inputs, dataset))
+
+    preprocessing_inputs = tf.keras.layers.Concatenate()(normalized_layer + category_encodings_layer)
+    return tf.keras.Model(symbolic_inputs, preprocessing_inputs)
--- titanic/titanic_survivals.py
+++ titanic/titanic_survivals.py
@@ -1,6 +1,7 @@
-import dataset_helpers
-import input_helpers
 import numpy as np
+
+from dataset_helpers import *
+from input_helpers import *
 
 np.printoptions(precision=3, suppress=True)
 
@@ -10,28 +11,22 @@ files_path = 'titanic'
 train_file_name = 'train.csv'
 label_column_name = 'Survived'
 
-titanic_data = dataset_helpers.download_dataset(
+titanic_data = download_dataset(
     dataset_url,
     dataset_path,
     files_path,
     train_file_name,
     print_dataset=True)
 
-titanic_cleared = dataset_helpers.drop_unusable_columns(
+titanic_cleared = drop_unusable_columns(
     titanic_data,
     ['PassengerId', 'Name', 'Embarked', 'Ticket'])
 
-titanic_features, titanic_label = dataset_helpers.split_into_features_and_label(
+titanic_features, titanic_label = split_into_features_and_label(
     titanic_cleared,
     label_column_name)
 
-dataset_helpers.print_dataset_info(titanic_features, 'Titanic features')
+print_dataset_info(titanic_features, 'Titanic features')
 
-symbolic_inputs = input_helpers.build_symbolic_inputs(titanic_features)
-numerical_inputs = input_helpers.extract_numerical_inputs(symbolic_inputs)
-string_inputs = input_helpers.extract_string_inputs(symbolic_inputs)
-
-normalized_layer = input_helpers.normalize_inputs(numerical_inputs, titanic_features)
-category_encodings_layer =list(input_helpers.create_category_encodings(string_inputs, titanic_features))
-
-preprocessed_inputs = normalized_layer + category_encodings_layer
+preprocessing_head = create_preprocessing_head(titanic_features)
+tf.keras.utils.plot_model(model=preprocessing_head, rankdir='LR', dpi=72, show_shapes=True)
